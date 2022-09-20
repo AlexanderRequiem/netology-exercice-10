e502fbb85420959074bba941638ebd206e3b74ec
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-09-20 09:22:42 +0600

    Lecture 1, task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+  const tmp = 2*a;
+  if (discriminant === 0) {
+    arr.push(-b/tmp);
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/tmp);
+    arr.push((-b - Math.sqrt(discriminant) )/tmp);
+  }
   return arr; // array
 }
 
