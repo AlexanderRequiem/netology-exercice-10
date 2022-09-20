3b0dd6d04ed42ab619aaaef90faec9874cd7ecbe
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-09-20 11:43:55 +0600

    Lecture 1, task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -20,5 +20,24 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   // код для задачи №2 писать здесь
 
-  return totalAmount;
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+  if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+
+  const creditBody = parseFloat(amount) - parseFloat(contribution);
+  const percentInMonth = parseFloat(percent) / 100.0 / 12.0;
+  const dateFrom = new Date();
+  const dateTo = new Date(date);
+  const countMonth = dateTo.getMonth() - dateFrom.getMonth() + (12 * (dateTo.getFullYear() - dateFrom.getFullYear()));
+  const anountInMonth = creditBody * (percentInMonth + (percentInMonth / (Math.pow(1 + percentInMonth, countMonth) - 1) ))
+
+  totalAmount = anountInMonth * countMonth;
+
+  return +totalAmount.toFixed(2);
 }
