eafb269ca7aa0e88fa798e6be70f5c7fef7a465b
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-09-21 17:46:21 +0600

    Lecture 3, task 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  
   // Ваш код
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  result = arr1.every((item, index) => item === arr2[index]);
 
   return result; // boolean
 }
