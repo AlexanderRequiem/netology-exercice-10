18ef68941b29a924766f8ff2183380edc1f11345
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-09-23 07:54:22 +0600

    Lecture 6, task 2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,3 +16,45 @@ function validateCount(value) {
         return e;
     }
 }
+
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a+b < c || b+c < a || c + a < b) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.A = a;
+        this.B = b;
+        this.C = c;
+    }
+
+    getPerimeter() {
+        return this.A + this.B + this.C;
+    }
+    
+    getArea() {
+        const p = 0.5 * this.getPerimeter();
+        const mul = p * (p - this.A) * (p - this.B) * (p - this.C);
+        const res = Math.sqrt(mul);
+        return +res.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        const result = new Triangle(a, b, c);
+        return result;
+    }
+    catch(e) {
+        let result = {
+            getArea : function() {
+                return 'Ошибка! Треугольник не существует';
+            },
+
+            getPerimeter : function() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+        return result;
+    }
+}
