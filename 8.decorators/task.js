312b5c0c4b5d2d09b41493aec50c4564f3ea84c2
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-09-28 09:30:07 +0600

    Lecture 8, task 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -15,13 +15,40 @@ function cachingDecoratorNew(func) {
     if (cache.length > 5) { 
       cache.shift();
     }
-    
+
     console.log("Вычисляем: " + result);
     return "Вычисляем: " + result; 
   }
 }
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, ms) {
   // Ваш код
+  let timerId = null;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (wrapper.flagFirstCall) {
+      wrapper.count++;
+      wrapper.flagFirstCall = false;
+      return func(...args);
+    }
+
+    if (timerId !== null) {
+      clearTimeout(timerId);
+    }
+
+    timerId = setTimeout(() => { 
+      wrapper.count++;
+      func.apply(this, args); 
+    }, ms);
+  }
+
+  wrapper.flagFirstCall = true;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
+
