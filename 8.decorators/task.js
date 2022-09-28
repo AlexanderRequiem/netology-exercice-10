224c5a9433c651bc6bc45778425d0b46b5b1c99b
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-09-28 09:00:04 +0600

    Lecture 8, task 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,24 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+  const cache = [];
+
+  return function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash.length===hash.length && item.hash === hash);
+    if (objectInCache !== undefined) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args); 
+    cache.push({hash: hash, value: result}); 
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+    
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result; 
+  }
 }
 
 
